54df0cea4b42bcae6e920c9ab77f3c441a7d7c6d
StarCobra <damienpoirot@gmail.com>
2023-03-22 14:42:56 +0100

    Feature: all fichier de TP

---
+++ fruit_salad/lib/fruitpreview.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_salad/screens/fruitdetailsscreen.dart';
+import 'fruit.dart';
+
+class FruitPreview extends StatelessWidget {
+  final Fruit fruit;
+  final String snack;
+  final String snackError;
+  final VoidCallback onTap;
+  final VoidCallback deleteFruit;
+
+  const FruitPreview({
+    super.key,
+    required this.fruit,
+    required this.onTap,
+    required this.deleteFruit,
+    required this.snack,
+    required this.snackError,
+  });
+
+  Route _createRoute() {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) =>
+          FruitDetailsScreen(
+              fruit: fruit,
+              deleteFruit: deleteFruit,
+              onTap: onTap,
+              snack: snack,
+              snackError: snackError),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: fruit.color, // couleur de fond du fruit
+        child: ListTile(
+          onTap: () {
+            Navigator.push(
+              context,
+              _createRoute(),
+            );
+          },
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Row(children: [
+                  Stack(children: [
+                    Container(
+                      height: 50, // hauteur du cercle
+                      width: 50, // largeur du cercle
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue, // couleur du cercle
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/${fruit.pic}', // chemin de l'image à afficher
+                      height: 48,
+                      width: 48, // largeur de l'image
+                    ),
+                  ]),
+                  const SizedBox(width: 10),
+                  Text(
+                    fruit.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ]),
+              ),
+              Row(
+                children: [
+                  /* Text(
+                    "${((fruit.price * fruit.clickCount).toStringAsFixed(2)).replaceAll('.', ',')} €",
+                    textAlign: TextAlign.right,
+                    style: const TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),*/
+                  TextButton(
+                      onPressed: () {
+                        onTap;
+                        final snackBar = SnackBar(
+                          duration: const Duration(milliseconds: 100),
+                          content: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('${fruit.name} ajouté(e) !',
+                                    style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.normal)),
+                              ]),
+                          backgroundColor: Colors.green,
+                          action: SnackBarAction(
+                            label: "X",
+                            textColor: Colors.white,
+                            onPressed: () {
+                              // Some code to undo the change.
+                            },
+                          ),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      },
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          const EdgeInsets.all(
+                              20.0), // Remplacez 10.0 par la valeur de votre choix
+                        ),
+                        shape: MaterialStateProperty.all<CircleBorder>(
+                            const CircleBorder()),
+                      ),
+                      child: const Icon(
+                        Icons.add_shopping_cart,
+                        color: Colors.black38,
+                        // Ajouter cette ligne pour définir la couleur
+                        size:
+                            30, // Ajouter cette ligne pour définir la taille),
+                      )),
+                  const SizedBox(width: 10),
+                  // espace entre le texte et le bouton
+                  /*        TextButton(
+                    onPressed: deleteFruit,
+                    child: const Icon(Icons.delete),
+                  ),*/
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
--- fruit_salad/lib/main.dart
+++ fruit_salad/lib/main.dart
@@ -1,115 +1,52 @@
 import 'package:flutter/material.dart';
+import 'fruitsmaster.dart';
+import 'fruit.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+  final List<Fruit> fruits = [
+    Fruit('Ananas', Colors.yellow, 3.70, 0, 'ananas.png'),
+    Fruit('Banane', Colors.yellow, 1.99, 0, 'banane.png'),
+    Fruit('Cassis', Colors.purple, 6.99, 0, 'cassis.png'),
+    Fruit('Citron', Colors.amberAccent, 2.08, 0, 'citron.png'),
+    Fruit('Citron Vert', Colors.greenAccent, 3.38, 0, 'citron-vert.png'),
+    Fruit('Fraise', Colors.red, 5.99, 0, 'fraise.png'),
+    Fruit('Framboise', Colors.redAccent, 2.50, 0, 'framboise.png'),
+    Fruit('Fruit de la Passion', Colors.purple.shade900, 14.00, 0, 'fruit-de-la-passion.png'),
+    Fruit('Goyave', Colors.pinkAccent, 8.90, 0, 'goyave.png'),
+    Fruit('Grenade', Colors.redAccent, 2.59, 0, 'grenade.png'),
+    Fruit('Kaki', Colors.orange, 2.50, 0, 'kaki.png'),
+    Fruit('Kiwi', Colors.green, 2.50, 0, 'kiwi.png'),
+    Fruit('Litchi', Colors.pink.shade700, 12.90, 0, 'litchi.png'),
+    Fruit('Mangue', Colors.amber, 6.50, 0, 'mangue.png'),
+    Fruit('Melon', Colors.yellow.shade600, 4.99, 0, 'melon.png'),
+    Fruit('Mure', Colors.purple.shade900, 6.40, 0, 'mure.png'),
+    Fruit('Myrtille', Colors.purple.shade900, 1.70, 0, 'myrtille.png'),
+    Fruit('Orange', Colors.orange, 1.35, 0, 'orange.png'),
+    Fruit('Pastèque', Colors.greenAccent, 4.99, 0, 'pasteque.png'),
+    Fruit('Pêche', Colors.orangeAccent, 2.99, 0, 'peche.png'),
+    Fruit('Poire', Colors.green, 1.48, 0, 'poire.png'),
+    Fruit('Pomelo', Colors.orange.shade400, 0.70, 0, 'pomelo.png'),
+    Fruit('Pomme', Colors.red, 1.20, 0, 'pomme.png'),
+  ];
+
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Fruit Salad',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: FruitsMaster(
+        fruits: fruits,
+      )
     );
   }
 }
